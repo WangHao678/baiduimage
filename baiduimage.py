d339e2551ff1a888d086c6813a17e291f8466951
Tedu <wanghao_WH@163.com>
2019-11-15 19:52:25 +0800

    second commit

--- baiduimage.py
+++ baiduimage.py
@@ -8,7 +8,7 @@ import re
 class BaiduImage(object):
     def __init__(self):
         self.url = 'https://image.baidu.com/search/index?tn=baiduimage&word={}'
-        self.headers = {'User-Agent':'Mozilla/5.0'}
+        self.headers = {'User-Agent':'User-Agent':'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 2.0.50727; SLCC2; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; Tablet PC 2.0; .NET4.0E)'}
 
     def get_image(self,url,word):
         html = requests.get(url=url,headers=self.headers).text
